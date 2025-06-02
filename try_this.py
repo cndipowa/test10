4636084b88a9fa5d4d2f267f9c57269960a30816
che <cndipowa@users.noreply.github.com>
2025-06-02 07:34:36 -0400

    Update try_this.py

--- try_this.py
+++ try_this.py
@@ -49,3 +49,46 @@ print("Headers:")
 print(response.headers)
 print("Response Body:")
 print(response.text)
+
+import requests
+
+url = "https://app.harness.io/v1/entities"
+params = {
+    "orgIdentifier": "string",
+    "projectIdentifier": "string",
+    "convert": "false",
+    "dry_run": "false"
+}
+headers = {
+    "Content-Type": "application/json",
+    "Harness-Account": "string",
+    "x-api-key": "YOUR_API_KEY_HERE"
+}
+payload = {
+    "yaml": """apiVersion: harness.io/v1
+kind: component
+type: Service
+identifier: my-sample-service
+name: my-sample-service
+owner: sample-owner
+spec:
+  lifecycle: experimental
+  ownedBy:
+    - group/sample-group
+metadata:
+  description: My Sample service.
+  annotations:
+    backstage.io/source-location: url:https://github.com/sample/sample/tree/main/harness/sample/
+    backstage.io/techdocs-ref: dir:.
+  links:
+    - title: Website
+      url: http://my-sample-website.com
+  tags:
+    - my-sample"""
+}
+
+response = requests.post(url, headers=headers, params=params, json=payload)
+
+print("Status Code:", response.status_code)
+print("Response Body:", response.text)
+
